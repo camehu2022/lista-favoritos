909ac3853cb920ba52ea4ffabeef948cb5661f30
camehu2022 <c.mehutc@hotmail.com>
2022-02-25 17:40:48 -0300

    del

--- db_orm.py
+++ db_orm.py
@@ -1,3 +1,4 @@
+from click import echo
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,8 +6,8 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://groyamyyesfoig:79348178c353aa396f007dae083911a6fdab68f0b193c353990559a9087ed9c7@postgresserver/db=dfisprtljet2ns"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+#SQLALCHEMY_DATABASE_URL = "postgres:// nxuknqjavcpxsf : aafd4ad7bd3c93b5a9b0116afe12191046f23c2c43d1610e5da3b6d7d5b1a8cd @ ec2-3-219-204-29.compute-1.amazonaws.com : 5432 / d1vujr12ut9es"
  
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -17,14 +18,13 @@ session = SessionLocal()
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "users"
+        
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    def __repr__(self):
-        return f'User {self.name}'
-    
+    hashed_password = Column(String)   
+        
     Base.metadata.create_all(engine)
+    
+   
